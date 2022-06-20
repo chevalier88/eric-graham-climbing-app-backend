148f72160c72e21b051943c18ed1b873d5843694
EricFooSD <fooshida@gmail.com>
2022-06-20 23:37:58 +0800

    added function to add new route to trip

--- controllers/routes.mjs
+++ controllers/routes.mjs
@@ -29,8 +29,31 @@ export default function initRoutesController(db) {
     }
   };
 
+  // add new route to ROUTES DB
+  const addNewRoute = async (request, response) => {
+    console.log(request.params.id);
+    try {
+      // construct route to be added
+      const newRoute = {
+        name: request.body.name,
+        difficulty: request.body.difficulty,
+        tripId: request.params.id,
+      };
+
+      // sequelize: create entry in DB
+      const newRouteAdded = await db.Route.create(newRoute);
+      console.log('did this controller work?');
+      console.log(newRouteAdded);
+      response.send(newRouteAdded);
+    } catch (error) {
+      console.log(error);
+      response.send(error);
+    }
+  };
+
   return {
     index,
     getRoutesForOneTrip,
+    addNewRoute,
   };
 }
